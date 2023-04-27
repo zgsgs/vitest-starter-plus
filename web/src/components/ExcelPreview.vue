1fb9813ea5fd36fa7ad46785b7a5dbbd7ca67076
zhao.gaosheng <zhao.gaosheng@nint.com>
2023-04-27 10:48:44 +0800

    feat: 对接excel预览

--- web/src/components/ExcelPreview.vue
+++ web/src/components/ExcelPreview.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import { HotTable } from '@handsontable/vue3'
+import { registerAllModules } from 'handsontable/registry'
+import 'handsontable/dist/handsontable.full.css'
+
+// register Handsontable's modules
+registerAllModules()
+
 /* 组件属性 */
 // interface Props {
 // }
@@ -11,24 +18,30 @@ defineOptions({ name: 'ExcelPreview' })
 /* Hooks */
 // const attrs = useAttrs()
 /* 状态数据 */
-// const state = reactive({})
+const hotSettings = reactive({
+  data: [
+    ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1'],
+    ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2', 'J2'],
+    ['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'I3', 'J3'],
+    ['A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'I4', 'J4'],
+    ['A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'I5', 'J5'],
+    ['A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'I6', 'J6'],
+  ],
+  colHeaders: true,
+  width: 'auto',
+  height: 'auto',
+  licenseKey: 'non-commercial-and-evaluation',
+})
 /* 其他数据 */
-
 /* 计算 */
-// computed(() => {})
 /* 监听 */
-// watch(() => {}, () => {})
 /* 事件 */
-// const onClick = () => {}
 /* 生命周期 */
-// onMounted(() => {})
 /* 函数 */
-// function render(params) {}
-// })
 </script>
 
 <template>
-  <div>dddd</div>
+  <HotTable :settings="hotSettings" />
 </template>
 
 <style scoped></style>
