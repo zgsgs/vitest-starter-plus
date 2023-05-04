c8ff23caa7a3736bc21019462b8a34e578479863
zhao.gaosheng <zhao.gaosheng@nint.com>
2023-05-04 21:40:19 +0800

    feat: 集成nuxt

---
+++ packages/admin/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    ['@nuxtjs/eslint-module', { /* module options */ }],
+  ],
+})
---
+++ packages/admin/pages/home/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+/* Hooks */
+/* 状态数据 */
+/* 其他数据 */
+
+/* 计算 */
+/* 监听 */
+/* 事件 */
+/* 生命周期 */
+/* 函数 */
+</script>
+
+<template>
+  <div class="text-green-500">
+    Home
+  </div>
+</template>
