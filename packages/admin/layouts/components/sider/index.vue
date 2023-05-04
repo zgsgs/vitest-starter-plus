f07d535f97cd2e6a81ca0ad6f45355a0cd2e4024
zhao.gaosheng <zhao.gaosheng@nint.com>
2023-05-04 21:40:45 +0800

    feat: 添加布局页面

---
+++ packages/admin/layouts/components/sider/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+/* 组件属性 */
+/* 组件事件 */
+/* 宏定义 */
+defineOptions({ name: 'index' })
+/* Hooks */
+/* 状态数据 */
+/* 其他数据 */
+
+/* 计算 */
+/* 监听 */
+/* 事件 */
+/* 生命周期 */
+/* 函数 */
+</script>
+
+<template>
+  <div>菜单</div>
+</template>
