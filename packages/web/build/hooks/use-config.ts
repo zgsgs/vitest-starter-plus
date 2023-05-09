838913c121b7d28d4b68ff9cbf684de825af1847
zhao.gaosheng <zhao.gaosheng@nint.com>
2023-05-09 19:07:40 +0800

    fix: router 迁移

--- packages/web/build/hooks/use-config.ts
+++ packages/web/build/hooks/use-config.ts
@@ -1,15 +1,14 @@
-export function useConfig(viteEnv: ImportMetaEnv = import.meta.env) {
-  const configEnable = {
-    open: 'Y',
-    close: 'N',
-  }
+export const configEnable = {
+  open: 'Y',
+  close: 'N',
+}
+
+export function useConfig(viteEnv: ImportMetaEnv) {
   const isOpenProxy = viteEnv.VITE_HTTP_PROXY === configEnable.open
   const isOpenVisualizer = viteEnv.VITE_VISUALIZER === configEnable.open
-  const isHashRoute = viteEnv.VITE_HASH_ROUTE ? viteEnv.VITE_HASH_ROUTE === configEnable.open : configEnable.close
 
   return {
     isOpenProxy,
     isOpenVisualizer,
-    isHashRoute,
   }
 }
