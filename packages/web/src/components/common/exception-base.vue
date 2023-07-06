c3d2407b4d5c1c2eccc84dbd18bd37deb134aff9
zhao.gaosheng <zhao.gaosheng@nint.com>
2023-07-06 18:28:33 +0800

    fix: 修复报错，添加qs包

--- packages/web/src/components/common/exception-base.vue
+++ packages/web/src/components/common/exception-base.vue
@@ -27,9 +27,9 @@ const isServiceError = computed(() => {
       <icon-local-service-error v-if="isServiceError" />
     </div>
     <router-link :to="{ name: routeHomePath }">
-      <button type="primary">
+      <n-button type="primary">
         回到首页
-      </button>
+      </n-button>
     </router-link>
   </div>
 </template>
